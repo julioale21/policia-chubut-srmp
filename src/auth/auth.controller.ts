a17e16e9f9eb4badb7387e15341bca42f4bd0509
julioale21 <30907777+julioale21@users.noreply.github.com>
2023-12-04 09:58:01 -0300

    added authentication and authorization to ingress controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,9 +2,6 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginUserDto } from './dto';
 
-import { Auth } from './decorators';
-import { ValidRoles } from './interfaces';
-
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
@@ -17,11 +14,5 @@ export class AuthController {
   @Post('login')
   login(@Body() loginUserDto: LoginUserDto) {
     return this.authService.login(loginUserDto);
-  }
-
-  @Get('testing-private-route')
-  @Auth()
-  testingPrivateRoute() {
-    return 'This is a private route';
   }
 }
